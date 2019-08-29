524894aa6eb4c82d2e2ef72548f6c8cb1adfb27d
Angus Morrison <contact@angus-morrison.com>
2019-08-29 13:47:05 +0100

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post
+    @@all = []
+
+    attr_accessor :title, :author
+
+    def initialize(title)
+        @title = title
+        @@all << self
+    end
+
+    def author_name()
+        self.author ? self.author.name : nil
+    end
+
+    def self.all()
+        @@all
+    end
+
+end
